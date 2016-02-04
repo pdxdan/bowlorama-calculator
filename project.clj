4a67971f3e7d665890b663113a7483208c440b23
Daniel Anolik <dan@anolik.net>
2016-02-04 15:42:48 -0800

    updated URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bowlorama-calculator "0.1.0-SNAPSHOT"
   :description "Part of the bowlorama demo application that calculates bowling scores based on ball history."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/pdxdan/bowlorama-calculator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
